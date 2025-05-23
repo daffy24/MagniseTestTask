41906f1f9c381a691707c9ee25937b4255dd0830
Dima <kalenyuk.dima05@gmail.com>
2025-05-24 00:57:43 +0300

    Add Docker configuration

--- MagniseTestTask/DTOs/AssetDto.cs
+++ MagniseTestTask/DTOs/AssetDto.cs
@@ -1,9 +1,9 @@
-﻿namespace MagniseTestTask.DTOs;
+namespace MagniseTestTask.DTOs;
 
 public class AssetDto
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Symbol { get; set; }
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Symbol { get; set; }
     
 }
--- MagniseTestTask/Models/Asset.cs
+++ MagniseTestTask/Models/Asset.cs
@@ -7,6 +7,6 @@ public class Asset
     [Key]
     public string Id { get; set; }
     public string Name { get; set; }
-    public string Symbol { get; set; }
+    public string? Symbol { get; set; }
     public string Provider { get; set; }
 }
